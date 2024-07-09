8e94c6a46fb350808bcc68f3241b6fe1696989d2
Fabricio Araújo <araujopa@gmail.com>
2024-07-09 10:20:03 -0300

    fix: apk

--- android/app/src/main/java/com/cidd_app/MainApplication.java
+++ android/app/src/main/java/com/cidd_app/MainApplication.java
@@ -1,5 +1,7 @@
 package com.cidd_app;
 
+import com.cidd_app.BuildConfig;
+
 import android.app.Application;
 import android.content.res.Configuration;
 import androidx.annotation.NonNull;
@@ -61,9 +63,9 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false;
-    }
+//    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
+//      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false;
+//    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
